676639a84e153b6fa8312a0a6c33a04eaa148a1f
Amira <amirashawkym23@gmail.com>
2021-10-04 10:38:57 +0200

    slideshow

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+// navbar
 function showMenu() {
     let nav = document.getElementsByTagName('nav');
     nav[0].classList.remove('hidden');
@@ -5,4 +6,29 @@ function showMenu() {
 function hideMenu() {
     let nav = document.getElementsByTagName('nav');
     nav[0].classList.add('hidden');
+}
+// slideshow
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("slide");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" slide-active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " slide-active";
 }
