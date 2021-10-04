9d0fb1f78e0fe192ecc1b7fda99608756d0db24e
Amira <amirashawkym23@gmail.com>
2021-10-04 08:31:46 +0200

    Create navbar

---
+++ scripts.js
@@ -0,0 +1,8 @@
+function showMenu() {
+    let nav = document.getElementsByTagName('nav');
+    nav[0].classList.remove('hidden');
+}
+function hideMenu() {
+    let nav = document.getElementsByTagName('nav');
+    nav[0].classList.add('hidden');
+}
